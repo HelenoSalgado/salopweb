8ea1ed6bcd4c0737fbed9ffdbba0e9583b88f390
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2025-10-01 08:30:46 -0300

    Atualizar o nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
       noExternal: ['@nuxt/content'] // Trata o módulo como não externo no SSR, reduzindo client bundle
     },
     build: {
-      //cssCodeSplit: false,
+      cssCodeSplit: false,
       rollupOptions: {
         output: {
           manualChunks(id) {
